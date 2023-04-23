4775bb74334da5d2547d823f473b23b55f0d3780
Mateo <54645934+TheodoreAI@users.noreply.github.com>
2023-04-23 15:00:19 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+// NAME
+// Project (N)
+// Description: 
+// References: 
+// Author:
+// Email: 
+// URL:
+
+
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+
+int main(){
+  
+  return 0; 
+  
+  
+}
